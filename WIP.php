6e6e8165386fb264f61a023950d2c027840849c2
Thorvald van Beek <tvanbeek@roc-dev.com>
2020-01-20 14:09:28 +0100

    pagina's aangemaakt

---
+++ WIP.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+
+<head>
+    <title>Paris RP</title>
+
+    <link href="Paris.png" rel="icon" type="image/png" />
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <div class="navbar">
+        <a href="index.htm"><button type="submit">Home</button></a>
+        <a href="news.php"><button type="submit">News</button></a>
+        <a href="WIP.php"><button type="submit">Photo's</button></a>
+        <a href="WIP.php"><button type="submit">Sign in</button></a>
+    </div>
+
+    <div class="Warning">
+        <h2><b>LET OP! DEZE SITE IS NOG IN ONDERHOUD<br> ER KOMEN LANGZAAM MEER DELEN BIJ</b></h2>
+    </div>
+
+</body>
+
+</html>
---
+++ news.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+
+<head>
+    <title>Paris RP</title>
+
+    <link href="Paris.png" rel="icon" type="image/png" />
+    <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+    <div class="navbar">
+        <a href="index.htm"><button type="submit">Home</button></a>
+        <a href="news.php"><button type="submit">News</button></a>
+        <a href="WIP.php"><button type="submit">Photo's</button></a>
+        <a href="WIP.php"><button type="submit">Sign in</button></a>
+    </div>
+
+    <div class="Warning">
+        <h2><b>LET OP! DEZE SITE IS NOG IN ONDERHOUD<br> ER KOMEN LANGZAAM MEER DELEN BIJ</b></h2>
+    </div>
+    <div class="video">
+<video src="wip.gif" loop autoplay mediagroup="gif" preload="auto" onload="autoplay">
+    </video>
+    </div>
+    <div  class="content">
+        <h1>Paris Roleplay</h1>
+        <p>
+            Welcome in paris. Paris is the 1e roleplay server van de hoofdstad van Frankrijk.
+        </p>
+    </div>
+    <div  class="breaking_news">
+        <h3 >
+            Vanmiddag zijn er HR interview's voor deze eenheden:</h3><br>
+        <p  class="Police"> Police.
+            <p  class="RAID"> Raid.
+                <p  class="Pompiers"> Pompiers.
+                    <p  class="Ambulance"> Ambulance.
+
+                        <h4 > De interviews beginnen om <b>16:00 t/m 17:00</b> <br> 
+                            Dan is er van 17:00 tot 18:00 een pauze<br> 
+                            En dan beginnen ze weer
+                            van<b>18:00 tot 19:00</b>.<br>
+                            Dus er zijn 2 rondes.
+    </div>
+</body>
+
+</html>
