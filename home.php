948c668577900336a534c12c3f6a3483346975e7
Thorvald van Beek <tvanbeek@roc-dev.com>
2020-03-05 22:30:54 +0100

    extra beveiliging toegevoegd, error (hopelijk) opgelost

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
 require 'connect.php';
-$stmt = $pdo->query("SELECT * FROM accounts WHERE naam = " . $_SESSION['username']);
+$stmt = $pdo->query("SELECT * FROM accounts WHERE naam = LIKE " . $_SESSION['username']);
 foreach ($stmt as $row) {
     $_SESSION['username'] = $row['naam'];
     $_SESSION['password'] = $row['Password'];
